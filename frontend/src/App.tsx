cb7f81df88b8771fe0f802bcaf321d3ae2f8d449
Nabil <nabil.roslizar@gmail.com>
2025-01-25 21:20:32 +1100

    update searchbar ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,15 +7,18 @@ import List from "./components/List"
 function App() {
 
   return (
-    <>
+    <div className="bg">
     <Header/>
        <HeroSection/>
-       <div className="mx-50 px-5 justify-center">
-        <Searchbar/>
-       </div>
+       <div className="flex items-center justify-between mx-50 px-5 mt-10">
+        <Searchbar />
+        <button className="bg-white text-gray-800 border border-gray-300 px-3 py-1 rounded hover:bg-gray-100 text-sm">
+            Filter
+        </button>
+      </div>
        <List/>
 
-    </>
+    </div>
   )
 }
 
--- frontend/src/components/Searchbar.tsx
+++ frontend/src/components/Searchbar.tsx
@@ -2,12 +2,15 @@ import TextField from "@mui/material/TextField";
 
 const Searchbar = () => {
   return (
-    <TextField
-      label="Search"
-      variant="outlined"
-      fullWidth
-      onChange={(e) => console.log(e.target.value)}
-    />
+    <div className="w-full sm:w-1/2 md:w-1/3">
+      <TextField
+        label="Search"
+        variant="outlined"
+        size="small" // Makes the TextField smaller
+        fullWidth
+        onChange={(e) => console.log(e.target.value)}
+      />
+    </div>
   );
 };
 
