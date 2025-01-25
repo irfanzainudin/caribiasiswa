f1f03898df8dfcbdb0be403778476788e816ed25
irfanzainudin <m.irfan.zain@gmail.com>
2025-01-25 10:24:16 +0800

    ocd

--- backend/caribiasiswa/settings.py
+++ backend/caribiasiswa/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # take environment variables from .env.
+load_dotenv()  # take environment variables from .env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
